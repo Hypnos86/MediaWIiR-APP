2c15e1277d3f611384cf8f58e3db03969960101a
Kamil Kubiak <kubiak.kamil23@gmail.com>
2023-08-03 14:57:12 +0200

    add files for gas and health media

---
+++ Models/GasResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaWIiR_APP.Models
+{
+    internal class GasResult
+    {
+        public int Heat { get; set; }
+        public int Power { get; set; }
+        public int Month { get; set; }
+        public int Carrier { get; set; }
+    }
+}
